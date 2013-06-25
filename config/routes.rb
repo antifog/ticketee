e2b14d27d746dbd08f4f67b37e0745edcdfaa88f
antifog <superchunkie@gmail.com>
2013-06-24 22:02:33 -0500

    Creation of projects feature complete

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,11 @@ Ticketee::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'projects#index'
+
+  Ticketee::Application.routes.draw do
+    resources :projects
+  end
 
   # See how all your routes lay out with "rake routes"
 
